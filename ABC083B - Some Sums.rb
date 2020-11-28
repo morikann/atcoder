ab2da0a1a81d2a7bac20301b3656574e02baf95e
morikann <mrknt0088@gmail.com>
2020-11-28 23:53:30 +0900

    close #9

---
+++ ABC083B - Some Sums.rb
@@ -0,0 +1,19 @@
+N, A, B = gets.chomp.split(' ').map(&:to_i)
+total = 0
+1.upto(N) do |i|
+  sum = findSumOfDigits(i)
+  if sum >= A && sum <= B
+    total += i
+  end
+end
+
+def findSumOfDigits(n)
+  sum = 0
+  while n > 0
+    sum += n % 10
+    n /= 10
+  end
+  return sum
+end
+
+puts total
