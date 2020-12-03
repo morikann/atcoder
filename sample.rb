7fd4036ffdaa3eb27a315e5d1e202116d7d33f0d
morikann <mrknt0088@gmail.com>
2020-12-03 22:13:28 +0900

    close #17

--- sample.rb
+++ sample.rb
@@ -1,6 +1,7 @@
-(1..9).each do |i|
-  (1..8).each do |j|
-    print "#{i*j} "
+n = gets.chomp.to_i
+(0..n).each do |i|
+  (0..n-i).each do |j|
+    print "#{i*10000 + j*5000 + (n - i - j)*1000} "
   end
   puts "\n"
 end
