5b47ee524eb642b191e797d747faa84a16b9c776
morikann <mrknt0088@gmail.com>
2021-03-30 22:00:34 +0900

    3/30

--- Ruby/ABC/197/C.rb
+++ Ruby/ABC/197/C.rb
@@ -1,8 +1,22 @@
-# p 10.to_s(2)
-n = gets.chomp.to_i
-array = gets.chomp.split.map(&:to_i)
+# n = gets.chomp.to_i
+# array = gets.chomp.split.map(&:to_i)
+# ans = 1 << 30
 
-if array.size == 1
-  puts array[0]
-else
-  
+# (0...(1 << (n - 1))).each do |s|
+#   now = 0
+#   o = 0
+#   (0...n).each do |i|
+#     o |= array[i]
+#     if ((s >> i) & 1) == 1
+#       now ^= o
+#       o = 0
+#     end
+#   end
+#   now ^= o
+#   ans = [ans, now].min
+# end
+
+# puts ans
+
+# 2 | 1 #=> 3
+# 00000010 | 00000001 #=> 00000011
