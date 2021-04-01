07b6f536e0d1723cdee6b02f158085a64690fa3a
morikann <mrknt0088@gmail.com>
2021-04-01 20:27:09 +0900

    194

--- Ruby/ABC/194/C.rb
+++ Ruby/ABC/194/C.rb
@@ -1,3 +1,5 @@
+# コンテストTLE回答
+
 # n = gets.chomp.to_i
 # a = gets.split.map(&:to_i)
 
@@ -10,26 +12,48 @@
 
 # puts sum
 
-n = gets.chomp.to_i
-array = gets.chomp.split.map(&:to_i)
 
-new_array = array.map do
-  _1 ** 2
-end
 
-x = n * new_array.sum
+# 回答1
 
-y = array.sum ** 2
+# n = gets.chomp.to_i
+# array = gets.chomp.split.map(&:to_i)
 
-puts x - y
-
-# x = n * a.sum do
+# new_array = array.map do
 #   _1 ** 2
-#   p _1 ** 2
 # end
-# p x
 
+# x = n * new_array.sum
 
-# y = a.sum ** 2
+# y = array.sum ** 2
 
 # puts x - y
+
+
+
+# 回答2
+
+# n = gets.chomp.to_i
+# a = gets.chomp.split.map(&:to_i)
+
+# MaxA = 200
+# d = []
+# ans = 0
+# (0...n).each do |i|
+#   (-MaxA..MaxA).each do |aj|
+#     c = d[MaxA + aj]
+#     x = a[i] - aj
+#     ans += x * x * c
+#   end
+#   d[MaxA + a[i]] += 1
+# end
+# puts ans
+n = gets.chomp.to_i
+a = gets.chomp.split.map(&:to_i)
+MaxA = 200
+d = []
+p d[MaxA*2+1]
+# (0...n).each do |i|
+#   d[MaxA + a[i]] += 1
+# end
+# p d
