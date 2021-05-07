0ebcac5b002199cf58674e5dff5c6416e743698d
morikann <mrknt0088@gmail.com>
2021-05-07 16:38:53 +0900

    python

---
+++ Python/Typical-90/004.py
@@ -0,0 +1,12 @@
+h, w = map(int, input().split())
+
+# 入力
+a = [list(map(int, input().split())) for i in range(h)]
+
+a = []
+for i in range(h):
+  a.append(list(map(int, input().split())))
+
+# 前計算
+print(a)
+
