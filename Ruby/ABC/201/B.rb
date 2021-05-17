e0a9f402be9e5618e3ac791c8ee4c12471c7dc2c
morikann <mrknt0088@gmail.com>
2021-05-17 11:36:24 +0900

    ABC201の振り返り

--- Ruby/ABC/201/B.rb
+++ Ruby/ABC/201/B.rb
@@ -1,3 +1,18 @@
+# n = gets.to_i
+# a = []
+
+# (0...n).each do |i|
+#   a << gets.split
+#   a[i][1] = a[i][1].to_i
+# end
+
+# a = a.sort { |a, b| a[1] <=> b[1]}
+
+# puts a[-2][0]
+
+
+
+## sort_byの方がわかりやすい
 n = gets.to_i
 a = []
 
@@ -6,6 +21,5 @@ a = []
   a[i][1] = a[i][1].to_i
 end
 
-a = a.sort { |a, b| a[1] <=> b[1]}
-
+a = a.sort_by {|x| x[1]}
 puts a[-2][0]
--- Ruby/ABC/201/C.rb
+++ Ruby/ABC/201/C.rb
@@ -1,37 +1,60 @@
-s = gets.chomp.chars
+## コンテストの回答 AC
+# s = gets.chomp.chars
 
-o = []
-q = []
-x = []
+# o = []
+# q = []
+# x = []
 
-s.each_with_index do |char, i|
-  o << i if char == 'o'
-  q << i if char == '?'
-  x << i if char == 'x'
+# s.each_with_index do |char, i|
+#   o << i if char == 'o'
+#   q << i if char == '?'
+#   x << i if char == 'x'
+# end
+
+# if o.length > 4
+#   puts 0
+# elsif o.length == 4
+#   cnt = 0
+#   o.repeated_permutation(4).to_a.each do |array|
+#     if (o - array == [])
+#       cnt += 1
+#     end
+#   end
+#   puts cnt
+# elsif x.length == 10
+#   puts 0
+# elsif q.length == 10
+#   puts q.repeated_permutation(4).to_a.length
+# else
+#   new_a = o + q
+#   cnt = 0
+#   new_a.repeated_permutation(4).to_a.each do |array|
+#     if (o - array == [])
+#       cnt += 1
+#     end
+#   end
+#   puts cnt
+# end
+
+
+## AtCoderLiveの回答
+s = gets.chomp.to_s
+
+ans = 0
+(0...10000).each do |num|
+  x = num
+  a = Array.new(10) {0}
+  (0...4).each do |i|
+    d = x % 10
+    a[d] = 1
+    x /= 10
+  end
+  flag = true
+  (0...10).each do |i|
+    flag = false if s[i] == 'o' && a[i] != 1
+    flag = false if s[i] == 'x' && a[i] != 0
+  end
+  ans += 1 if flag
 end
 
-if o.length > 4
-  puts 0
-elsif o.length == 4
-  cnt = 0
-  o.repeated_permutation(4).to_a.each do |array|
-    if (o - array == [])
-      cnt += 1
-    end
-  end
-  puts cnt
-elsif x.length == 10
-  puts 0
-elsif q.length == 10
-  puts q.repeated_permutation(4).to_a.length
-else
-  new_a = o + q
-  cnt = 0
-  new_a.repeated_permutation(4).to_a.each do |array|
-    if (o - array == [])
-      cnt += 1
-    end
-  end
-  puts cnt
-end
-
+puts ans
