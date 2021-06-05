c969591ee018da46e95becc5ff7b41b6feedc0b0
morikann <mrknt0088@gmail.com>
2021-06-05 10:39:45 +0900

    典型22

--- Ruby/Typical-90/star2/022.rb
+++ Ruby/Typical-90/star2/022.rb
@@ -1,19 +1,22 @@
-a, b, c = map(int, input().split())
+a, b, c = gets.split.map(&:to_i)
 
-def gcd(x, y):
-  if x >= y:
-    while x % y != 0:
+def gcd(x, y)
+  if x >= y
+    while x % y != 0
       mod = x % y
       x = y
       y = mod
+    end
     return y
-  elif y > x:
-    while y % x != 0:
+  elsif y > x
+    while y % x != 0
       mod = y % x
       y = x
       x = mod
+    end
     return x
-
+  end
+end
 r = gcd(a, gcd(b, c))
 cnt = (a / r - 1) + (b / r - 1) + (c / r - 1)
-print(cnt)
+puts cnt
