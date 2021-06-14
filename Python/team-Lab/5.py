0d08dc43c4e1822844e56e43e3691cfd9526cd51
morikann <mrknt0088@gmail.com>
2021-06-14 21:58:41 +0900

    TeamLab

---
+++ Python/team-Lab/5.py
@@ -0,0 +1,15 @@
+# 最後に全部が0の時を除く
+# ans = 31 * 41 * 21 - 1
+
+# print(ans)
+
+# array = []
+
+# for i in range(31):
+#   for j in range(41):
+#     for k in range(21):
+#       ans = 205*i + 82*j + 30*k
+#       if not ans in array:
+#         array.append(ans)
+
+# print(len(array))
