761aae4a07a054cbfa47ac5933233cecea228638
morikann <mrknt0088@gmail.com>
2021-06-16 23:56:18 +0900

    ABC205D

--- Python/ABC/205/D.py
+++ Python/ABC/205/D.py
@@ -1,16 +1,20 @@
-# N, Q = map(int, input().split())
-# A = map(int, input().split())
-# K = []
-# for i in range(Q):
-#   ki = int(input())
-#   K.append(ki)
+N, Q = map(int, input().split())
+A = map(int, input().split())
+K = []
+C = [0] * N
+for i in range(N):
+  C[i] = A[i]-i-1
+for i in range(Q):
+  ki = int(input())
+  K.append(ki)
 
-# array = []
-# for i in range(N):
-#   if not i in A:
-#     array.append(i)
+array = []
+for i in range(N):
+  if not i in A:
+    array.append(i)
 
-# for i in K:
-#   print(array[i-1])
-import math
-print(math.log2(10 ** 18))
+for i in K:
+  print(array[i-1])
+
+# import math
+# print(math.log2(10 ** 18))
